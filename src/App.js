a497570cb2b51ccb84fbcfa7bcd67085f3492a78
jkkumawat48 <jkkumawat48@gmail.com>
2022-08-31 10:31:50 +0530

    Routing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactDOM } from 'react';
+import Header from './Component/LeyOut/Header';
+import Footer from './Component/LeyOut/Footer';
+import Nav from './Component/LeyOut/Nav';
+import Home from './Component/LeyOut/Home'
+import About from './Component/LeyOut/About'
+import Form from './Component/LeyOut/Form'
+import {BrowserRouter as Router, Route,Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header/>
+      <Footer/>
+           <Nav/> 
+                      <Home/>
+            <Form/>
     </div>
   );
 }
